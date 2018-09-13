af59d0a90689133e31e001eb85299312e644c3a2
estilwell <31806580+estilwell@users.noreply.github.com>
2018-09-13 13:23:12 -0400

    Update silly.sh

--- silly.sh
+++ silly.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-import geni.rspec.pg as pg
-
-node.addService(pg.Execute(shell = "???", command = "sudo apt-get update")
-node.addService(pg.Execute(shell = "???", command = "sudo apt-get install apache2")
+sudo yum -y update
+sudo yum install httpd
 
 echo "This is a silly script" > /tmp/silly.txt
 
