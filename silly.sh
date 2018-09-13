f8eba8ee26cda841ed3cf81fd2ce1fdb6b5cbeb7
estilwell <31806580+estilwell@users.noreply.github.com>
2018-09-13 11:23:56 -0400

    Update silly.sh

--- silly.sh
+++ silly.sh
@@ -3,7 +3,7 @@
 import geni.rspec.pg as pg
 
 node.addService(pg.Execute(shell = "???", command = "sudo apt-get update")
-node.addService(pg.Execute(shell = "???", command = "audo apt-get install apache2")
+node.addService(pg.Execute(shell = "???", command = "sudo apt-get install apache2")
 
 echo "This is a silly script" > /tmp/silly.txt
 
