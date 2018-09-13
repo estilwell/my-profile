8b1f817b8101c5115ce4e532d9a2e4856d00f52b
estilwell <31806580+estilwell@users.noreply.github.com>
2018-09-13 14:31:33 -0400

    Update silly.sh

--- silly.sh
+++ silly.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 sudo yum -y update
-sudo yum install httpd
 
-	
 sudo firewall-cmd --permanent --add-port=80/tcp
+sudo firewall-cmd --permanent --add-port=443/tcp
 sudo firewall-cmd --reload
+
+sudo yum install httpd
 
 sudo systemctl start httpd
 sudo systemctl enable httpd
