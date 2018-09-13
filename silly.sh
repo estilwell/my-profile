ac9a6c86d51d139638396ee0d44e5584a8839ab9
estilwell <31806580+estilwell@users.noreply.github.com>
2018-09-13 15:40:22 -0400

    Update silly.sh

--- silly.sh
+++ silly.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
-sudo yum -y update
+echo "This is a silly script" > /tmp/silly.txt
 
+sudo yum clean all
+sudo yum -y update
+sudo yum -y install httpd
 sudo firewall-cmd --permanent --add-port=80/tcp
 sudo firewall-cmd --permanent --add-port=443/tcp
 sudo firewall-cmd --reload
-
-sudo yum -y install httpd
-
 sudo systemctl stop httpd.service
 sudo systemctl start httpd.service
 sudo systemctl enable httpd.service
-
-echo "This is a silly script" > /tmp/silly.txt
 
